d7b5d0c8bc0da3d3965b2a069a9c3ffbc441b615
hari <hari.harmalkar@gmail.com>
2018-04-09 11:36:57 +0530

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import DashboardPage from './components/pages/DashboardPage';
 
 const App = () => 
 <div className="ui container">
 <Route path="/" exact component={HomePage} />
 <Route path="/login" exact component={LoginPage} />
+<Route path="/dashboard" exact component={DashboardPage} />
 </div>;
 
 export default App;
