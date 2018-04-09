6f6196e1a89602043e23d1a7e5d347802a78fcc2
hari <hari.harmalkar@gmail.com>
2018-04-09 15:08:58 +0530

    signup with auto login

--- src/api.js
+++ src/api.js
@@ -5,6 +5,6 @@ export default {
 		login : (credentials) => 
 		axios.post('/login', credentials).then(res => res.data.user),
 		signup : (user) => 
-		axios.post('/users/sign-up', user).then(res => res.data)
+		axios.post('/users/sign-up', user).then(res => res.data.user)
 	}
 }
