039be059de0c996274e23533c5e7196e7d406bf8
hari <hari.harmalkar@gmail.com>
2018-04-09 11:51:52 +0530

    higher order component for dashboard route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Route } from 'react-router-dom';
 import React from 'react';
 
+import DashboardPage from './components/pages/DashboardPage';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
-import DashboardPage from './components/pages/DashboardPage';
+import UserRoute from './components/routes/UserRoute';
 
 const App = () => 
 <div className="ui container">
 <Route path="/" exact component={HomePage} />
 <Route path="/login" exact component={LoginPage} />
-<Route path="/dashboard" exact component={DashboardPage} />
+<UserRoute path="/dashboard" exact component={DashboardPage} />
 </div>;
 
 export default App;
---
+++ src/components/routes/UserRoute.js
@@ -0,0 +1,14 @@
+import { Route } from 'react-router-dom';
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const UserRoute = ({component:Component, ...rest}) => (
+       <Route {...rest} render={props => <Component {...props} />} /> 
+ );
+
+UserRoute.propTypes = {
+    component: PropTypes.func.isRequired
+};
+
+export default UserRoute;
